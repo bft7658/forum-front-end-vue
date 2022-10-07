7fcc56a535764d9cb415942f1685c0aa91da00ae
bft7658 <bft7658@gmail.com>
2022-10-07 21:48:05 +0800

    add User.vue

---
+++ src/components/UserCommentsCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>2</strong> 已評論餐廳
+    </div>
+    <div class="card-body">
+      <a href="/restaurants/161">
+        <img src="https://i.imgur.com/8v5PQc4.jpeg" width="60" height="60" class="avatar">
+      </a>
+      <a href="/restaurants/171">
+        <img src="https://i.imgur.com/qlQLHHU.jpeg" width="60" height="60" class="avatar">
+      </a>
+    </div>
+  </div>
+</template>
---
+++ src/components/UserFavoritedRestaurantsCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="card">
+    <div class="card-header">
+      <strong>3</strong> 收藏的餐廳
+    </div>
+    <div class="card-body">
+      <a href="/restaurants/171">
+        <img src="https://i.imgur.com/qlQLHHU.jpeg" width="60" height="60" class="avatar">
+      </a>
+      <a href="/restaurants/131">
+        <img src="https://i.imgur.com/zHOtfMW.jpeg" width="60" height="60" class="avatar">
+      </a>
+      <a href="/restaurants/151">
+        <img src="https://i.imgur.com/Tvvai2K.jpeg" width="60" height="60" class="avatar">
+      </a>
+    </div>
+  </div>
+</template>
