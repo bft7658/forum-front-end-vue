5a712752399cf4be5e9cc89bdebf52c133565d84
bft7658 <bft7658@gmail.com>
2022-10-13 10:49:19 +0800

    add API - addFavorite & deleteFavorite, addLike & deleteLike

--- src/components/RestaurantCard.vue
+++ src/components/RestaurantCard.vue
@@ -16,16 +16,16 @@
         </p>
       </div>
       <div class="card-footer">
-        <button v-if="restaurant.isFavorited" @click.stop.prevent="deleteFavorite" type="button" class="btn btn-danger btn-border favorite mr-2">
+        <button v-if="restaurant.isFavorited" @click.stop.prevent="deleteFavorite(restaurant.id)" type="button" class="btn btn-danger btn-border favorite mr-2">
           移除最愛
         </button>
-        <button v-else @click.stop.prevent="addFavorite" type="button" class="btn btn-primary btn-border favorite mr-2">
+        <button v-else @click.stop.prevent="addFavorite(restaurant.id)" type="button" class="btn btn-primary btn-border favorite mr-2">
           加到最愛
         </button>
-        <button v-if="restaurant.isLiked" @click.stop.prevent="deleteLike" type="button" class="btn btn-danger like mr-2">
+        <button v-if="restaurant.isLiked" @click.stop.prevent="deleteLike(restaurant.id)" type="button" class="btn btn-danger like mr-2">
           Unlike
         </button>
-        <button v-else @click.stop.prevent="addLike" type="button" class="btn btn-primary like mr-2">
+        <button v-else @click.stop.prevent="addLike(restaurant.id)" type="button" class="btn btn-primary like mr-2">
           Like
         </button>
       </div>
@@ -35,6 +35,8 @@
 
 <script>
 import { emptyImageFilter } from './../utils/mixins'
+import usersAPI from './../apis/users'
+import { Toast } from './../utils/helpers'
 
 export default {
   mixins: [emptyImageFilter],
@@ -50,29 +52,79 @@ export default {
     }
   },
   methods: {
-    addFavorite () {
-      this.restaurant = {
-        // 保留餐廳內原有資料
-        ...this.restaurant,
-        isFavorited: true
+    async addFavorite(restaurantId) {
+      try {
+        const { data } = await usersAPI.addFavorite({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.restaurant = {
+          // 保留餐廳內原有資料
+          ...this.restaurant,
+          isFavorited: true
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法將餐廳加入最愛，請稍後再試'
+        })
+        console.log('error', error)
       }
     },
-    deleteFavorite() {
-      this.restaurant = {
-        ...this.restaurant,
-        isFavorited: false
+    async deleteFavorite(restaurantId) {
+      try {
+        const { data } = await usersAPI.deleteFavorite({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isFavorited: false
+        }
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法將餐廳移除最愛，請稍後再試'
+        })
+        console.log('error', error)
       }
     },
-    addLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: true
+    async addLike(restaurantId) {
+      try {
+        const { data } = await usersAPI.addLike({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: true
+        }
+      } catch {
+        Toast.fire({
+          icon: 'error',
+          title: '無法對餐廳按讚，請稍後再試'
+        })
       }
     },
-    deleteLike() {
-      this.restaurant = {
-        ...this.restaurant,
-        isLiked: false
+    async deleteLike(restaurantId) {
+      try {
+        const { data } = await usersAPI.deleteLike({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.restaurant = {
+          ...this.restaurant,
+          isLiked: false
+        }
+      } catch {
+        Toast.fire({
+          icon: 'error',
+          title: '無法對餐廳取消按讚，請稍後再試'
+        })
       }
     }
   }
