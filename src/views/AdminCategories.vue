c5253a938bbfd53df22573c9d0893da80badfbb2
bft7658 <bft7658@gmail.com>
2022-10-14 10:12:21 +0800

    fix: integrate get admin categories

--- src/views/AdminCategories.vue
+++ src/views/AdminCategories.vue
@@ -62,35 +62,8 @@
 <script>
 import { v4 as uuidv4 } from 'uuid'
 import AdminNav from './../components/AdminNav.vue'
-
-const dummyData = {
-  categories: [
-    {
-      id: 1,
-      name: '中式料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 2,
-      name: '日本料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 3,
-      name: '義大利料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    },
-    {
-      id: 4,
-      name: '墨西哥料理',
-      createdAt: '2019-06-22T09:00:43.000Z',
-      updatedAt: '2019-06-22T09:00:43.000Z'
-    }
-  ]
-}
+import adminAPI from './../apis/admin'
+import { Toast } from './../utils/helpers'
 
 export default {
   components: {
@@ -106,12 +79,24 @@ export default {
     this.fetchCategories()
   },
   methods: {
-    fetchCategories() {
-      this.categories = dummyData.categories.map(category => ({
-        ...category,
-        isEditing: false,
-        nameCached: ''
-      }))
+    async fetchCategories() {
+      try {
+        const { data } = await adminAPI.categories.get()
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        this.categories = data.categories.map(category => ({
+          ...category,
+          isEditing: false,
+          nameCached: ''
+        }))
+      } catch (error) {
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳類別，請稍後再試'
+        })
+      }
     },
     createCategory() {
       this.categories.push({
