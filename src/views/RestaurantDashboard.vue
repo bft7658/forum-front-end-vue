f5aeedb2fcd8da75c61ea55ae09ee043201303b6
bft7658 <bft7658@gmail.com>
2022-10-15 15:39:55 +0800

    integrate API of RestaurantDashboard

--- src/views/RestaurantDashboard.vue
+++ src/views/RestaurantDashboard.vue
@@ -21,83 +21,9 @@
 </template>
 
 <script>
-const dummyData = {
-  "restaurant": {
-    "id": 1,
-    "name": "Stephanie Kozey",
-    "tel": "(300) 390-7270 x3608",
-    "address": "50907 Hegmann Street",
-    "opening_hours": "08:00",
-    "description": "Quia consequatur fugiat beatae velit ipsum est perferendis nostrum libero. Nobis sit aspernatur repellendus modi ea.",
-    "image": "https://loremflickr.com/320/240/restaurant,food/?random=68.27400712675275",
-    "viewCounts": 1,
-    "createdAt": "2022-10-04T13:09:52.000Z",
-    "updatedAt": "2022-10-04T13:12:58.000Z",
-    "CategoryId": 1,
-    "Category": {
-      "id": 1,
-      "name": "中式料理",
-      "createdAt": "2022-10-04T13:09:52.000Z",
-      "updatedAt": "2022-10-04T13:09:52.000Z"
-    },
-    "Comments": [
-      {
-        "id": 51,
-        "text": "Ipsum repudiandae fugit quisquam.",
-        "UserId": 1,
-        "RestaurantId": 1,
-        "createdAt": "2022-10-04T13:09:52.000Z",
-        "updatedAt": "2022-10-04T13:09:52.000Z",
-        "User": {
-          "id": 1,
-          "name": "root",
-          "email": "root@example.com",
-          "password": "$2a$10$Ez4oG5iECaJqMFkWrkU1Ne9sRTEzisIVSYE3/P07fpyNlpEC83sNy",
-          "isAdmin": true,
-          "image": null,
-          "createdAt": "2022-10-04T13:09:52.000Z",
-          "updatedAt": "2022-10-04T13:09:52.000Z"
-        }
-      },
-      {
-        "id": 101,
-        "text": "Praesentium atque delectus rerum.",
-        "UserId": 1,
-        "RestaurantId": 1,
-        "createdAt": "2022-10-04T13:09:52.000Z",
-        "updatedAt": "2022-10-04T13:09:52.000Z",
-        "User": {
-          "id": 1,
-          "name": "root",
-          "email": "root@example.com",
-          "password": "$2a$10$Ez4oG5iECaJqMFkWrkU1Ne9sRTEzisIVSYE3/P07fpyNlpEC83sNy",
-          "isAdmin": true,
-          "image": null,
-          "createdAt": "2022-10-04T13:09:52.000Z",
-          "updatedAt": "2022-10-04T13:09:52.000Z"
-        }
-      },
-      {
-        "id": 1,
-        "text": "Voluptatum ut facere ut dolor.",
-        "UserId": 2,
-        "RestaurantId": 1,
-        "createdAt": "2022-10-04T13:09:52.000Z",
-        "updatedAt": "2022-10-04T13:09:52.000Z",
-        "User": {
-          "id": 2,
-          "name": "user1",
-          "email": "user1@example.com",
-          "password": "$2a$10$k2KMcDkKZ9S6vCzCXs6uEOZ/lf.Hhd4XjBnAw8aO7CrwlwDdThPy.",
-          "isAdmin": false,
-          "image": null,
-          "createdAt": "2022-10-04T13:09:52.000Z",
-          "updatedAt": "2022-10-04T13:09:52.000Z"
-        }
-      }
-    ]
-  }
-}
+import restaurantsAPI from './../apis/restaurants'
+import { Toast } from './../utils/helpers'
+
 export default {
   data() {
     return {
@@ -110,22 +36,38 @@ export default {
       }
     }
   },
+  beforeRouteUpdate(to, from, next) {
+    const { id: restaurantId } = to.params
+    this.fetchRestaurant(restaurantId)
+    next()
+  },
   created() {
     const { id: restaurantId } = this.$route.params
     this.fetchRestaurant(restaurantId)
   },
   methods: {
-    fetchRestaurant(restaurantId) {
-      console.log(restaurantId)
-      const { restaurant } = dummyData
-      const { id, name, Category, Comments, viewCounts } = restaurant
-      this.restaurant = {
-        ...this.restaurant,
-        id,
-        name,
-        categoryName: Category ? Category.name : '未分類',
-        commentsLength: Comments.length,
-        viewCounts
+    async fetchRestaurant(restaurantId) {
+      try {
+        const { data } = await restaurantsAPI.getRestaurant({ restaurantId })
+        if (data.status === 'error') {
+          throw new Error(data.message)
+        }
+
+        const { id, name, Category, Comments, viewCounts } = data.restaurant
+        this.restaurant = {
+          ...this.restaurant,
+          id,
+          name,
+          categoryName: Category ? Category.name : '未分類',
+          commentsLength: Comments.length,
+          viewCounts
+        }
+      } catch (error) {
+        console.error(error.message)
+        Toast.fire({
+          icon: 'error',
+          title: '無法取得餐廳資料，請稍後再試'
+        })
       }
     }
   }
